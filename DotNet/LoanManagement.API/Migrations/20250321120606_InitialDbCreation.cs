179b6afd0f4dda95e40ba529547c04f8f4dd1264
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-03-21 16:30:29 +0000

    further staged changes in DotNet and NodeJS

---
+++ DotNet/LoanManagement.API/Migrations/20250321120606_InitialDbCreation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password", "Role" },
+                values: new object[] { new Guid("5aab83f7-ae58-4014-8560-acbc9c418a35"), "admin@loanmanagement.com", "Admin User", "$2a$11$i0cyvXi72Ic1JL/MWKc04eQ/g4CNRH6Z6g48K1yrdDikzDUcLe5GK", 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
